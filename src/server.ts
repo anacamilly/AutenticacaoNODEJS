bcb8cc9e02eaef3489f09c499800186d8b4e4d02
Ana Camilly <camillyguedes2@gmail.com>
2022-04-10 20:50:37 -0300

    Cleaning the code and reorganizing the app

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,23 @@
 import express from 'express';
-//--import express from 'express';-- ou --const express = require('express');--
 import path from 'path';
 
-//Inicializa o express (Framework web)
+//Inicializa o express
 const app = express();
+const port = process.env.PORT || 3000;
 
-import {homeRouter} from './routes';
+import authRoutes from './routes/auth';
+import pagesRoutes from './routes/pages';
 
 //View Engine - EJS
 app.set('views', path.join(__dirname, 'views')); //-> src/views
 app.set('view engine', 'ejs');
 
-app.use('/', homeRouter);
+app.use('/', pagesRoutes);
+app.use('/auth', authRoutes);
 
 //Inicia o servidor do express
-app.listen(3000, () => console.log('Server running on port 3000.'));
+app.listen(port, () => console.log(`Server running on port ${port}.`));
 
 
-/*//request -> cliente envia para o servidor
+//request -> cliente envia para o servidor
 //response -> servidor envia para o cliente
-app.get('/', (request, response) => {
-    response.send('OLA MUNDO')
-})*/
